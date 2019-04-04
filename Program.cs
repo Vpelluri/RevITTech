1dd1d81a8cbd37ba5cb67a2c157af4cdd935e439
Vpelluri <Vedavyas.P@Gmail.Com>
2019-04-04 21:05:58 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Xml;
+namespace WeatherAppTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> oDicTempData = new Dictionary<string, string>();
+            XmlDocument oXMLDocument = null;
+            var CurrentURL = @"http://api.openweathermap.org/data/2.5/forecast?id=6619279&appid=67d56d35122a25324119980946fd31d2&mode=xml";
+            using (WebClient oClient = new WebClient())
+            {
+                string xmlContent = oClient.DownloadString(CurrentURL);
+                oXMLDocument = new XmlDocument();
+                oXMLDocument.LoadXml(xmlContent);
+                
+                 XmlNodeList lstNodeTemp = oXMLDocument.SelectNodes("//temperature");
+                foreach(XmlNode oNode in lstNodeTemp)
+                {
+                    XmlAttribute oAttrTempValue = oNode.Attributes["value"];
+                    double dKelvin = double.Parse(oAttrTempValue.Value, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
+                    double dCelcius = dKelvin - 273.15;
+                    if (dCelcius > 25)
+                    {
+                        XmlNode oParentNode = oNode.ParentNode;
+                        if (oParentNode != null)
+                        {
+                            XmlAttribute oToDateTime = oParentNode.Attributes["to"];
+                            if (oToDateTime != null)
+                            {
+                                string date = oToDateTime.Value;
+                                if (date.Length > 10)
+                                {
+                                    date = date.Substring(0, 10);
+                                    if (!oDicTempData.ContainsKey(date))
+                                        oDicTempData.Add(date, (dCelcius.ToString()));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Number of days with temperature > 20 degrees is " + oDicTempData.Count);
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,7 @@
+-> The outcome of this solution is to identify the number of days in Sydney where the temperature is predicated to be above 20 degrees in the next 5 days
+
+Steps to view the outcome :
+
+1) Clone the solution to your local visual studio enviromnet using the git repository link shared in the email
+2) After successfully cloning to local environment, Build and run the solution
+3) The console window will get opened with weather information about the number of days predicited to be greater than 20 degrees.
